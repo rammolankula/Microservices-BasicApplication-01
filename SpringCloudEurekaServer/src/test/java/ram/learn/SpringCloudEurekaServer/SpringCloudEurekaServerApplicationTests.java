e641ad61928af788efad0e3afb7137c44bfbc0a7
Ram <53596726+rammolankula@users.noreply.github.com>
2024-06-20 15:59:42 +0530

    Add files via upload

---
+++ SpringCloudEurekaServer/src/test/java/ram/learn/SpringCloudEurekaServer/SpringCloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ram.learn.SpringCloudEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
